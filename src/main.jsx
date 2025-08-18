2f5c141663ac3a5ecf5e0d78df893fd3aa2ad4f7
Sualeh Siddiqui <sualehsiddiqui01@gmail.com>
2025-08-18 18:36:16 +0500

    back to previous restriction

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RestrictUser } from './Components/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RestrictUser>
+      <App />
+    </RestrictUser>
   </StrictMode>,
 )
